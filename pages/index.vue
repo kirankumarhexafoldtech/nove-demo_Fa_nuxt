6a322ca2b16a63fa25aeb63a334467f6aafd69f3
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-06-12 16:54:56 +0530

    created nuxt using nuxt-create

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+
+    <div class="py-4 container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+          <KpiComponents class="z-index-2"/>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import KpiComponents from "@/components/cards/KpiComponents.vue"
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    KpiComponents
+  }
+})
+</script>
