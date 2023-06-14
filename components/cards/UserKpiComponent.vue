907690853961c1ff2295d550bb93c93def4b8c37
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-06-14 11:14:35 +0530

    user kpi cards added

---
+++ components/cards/UserKpiComponent.vue
@@ -0,0 +1,80 @@
+<template>
+
+    <div class="mb-3 card user-card " :class="usercard">
+        <div class="p-3 ">
+            <div class="d-flex flex-row p-3 justify-content-around flex-gap-20" v-if="name">
+                <div class="align-middle">
+                    <slot name="profile"></slot>
+                </div>
+                <div class="h-25 d-flex flex-column pt-2 w-75" v-if="name">
+                    <span class=" text-l h5" style="font-weight:600">
+                        {{ name }}
+                    </span>
+                    <span class="lh-1 text-muted fw-lighter text-sm font-weight-bold position-relative start-3" v-if="roll">
+                        {{ roll }}
+                    </span>
+                </div>
+                <div class="mt-2 pt-2 text-sm d-flex">
+                    <ActiveIcon :fill="status ? 'green' : 'red'" class="radio-icon-position" /> Active
+                </div>
+            </div>
+
+            <div v-if="userdetails">
+                <div>
+                    <div class="text-md font-weight-bold">ID:101</div>
+                    <div class="row">
+                        <div class="col">
+                            <span class="text-sm text-muted">joining since: </span>
+                            <span class="text-sm font-weight-bold">2020/12/15</span>
+                        </div>
+                        <div class="col">
+                            <span class="text-sm text-muted">Email: </span>
+                            <span class="text-sm font-weight-bold">yawgraham@gmail.com</span>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col">
+                            <span class="text-sm text-muted">Phone number: </span>
+                            <span class="text-sm font-weight-bold">0277400000</span>
+                        </div>
+                        <div class="col">
+                            <span class="text-sm text-muted">Subzone: </span>
+                            <span class="text-sm font-weight-bold">N12</span>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col">
+                            <span class="text-sm text-muted">Zone: </span>
+                            <span class="text-sm font-weight-bold">Northen</span>
+                        </div>
+                        <div class="col">
+                            <span class="text-sm text-muted"> Address: </span>
+                            <span class="text-sm font-weight-bold">Agbogra-Ashongman</span>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</template>
+
+<script>
+import ActiveIcon from '../icons/ActiveIcon.vue';
+
+export default {
+name: "MiniStatisticsCard",
+components: { ActiveIcon },
+props:{
+    name:String,
+    roll:String,
+    status:Boolean,
+    usercard:String,
+    userdetails:Boolean
+}
+};
+</script>
+
+<style scoped></style>
---
+++ components/icons/ActiveIcon.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" :fill="fill" class="bi bi-dot" viewBox="0 0 16 16">
+  <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+</svg>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"ActiveIcon",
+    props:{
+        fill:{
+            type:String
+        }
+    }
+}
+</script>
